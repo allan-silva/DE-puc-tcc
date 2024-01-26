bb51535566527c0e2cdb0b9fc03915569738ba64
Allan Silva <allan@allansilva.com.br>
2024-01-26 01:51:13 -0300

    Make bucket regional

--- terraform/buckets/main.tf
+++ terraform/buckets/main.tf
@@ -2,5 +2,6 @@ resource "google_storage_bucket" "lake-bucket" {
     name = var.bucket_name
     location = var.location
     force_destroy = true
+    storage_class = "REGIONAL"
     public_access_prevention = "enforced"
 }
